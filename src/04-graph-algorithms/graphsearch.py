3d5454ad3ba63a8720748b794394ededd426eb01
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2019-05-08 23:02:54 +0200

    added breadth first with trackback

--- src/04-graph-algorithms/graphsearch.py
+++ src/04-graph-algorithms/graphsearch.py
@@ -1,5 +1,14 @@
 import graph as gr
 
+def track_back(path_vertices, start_vertex, target_vertex):
+    path = [target_vertex]
+    current_step = target_vertex
+    while current_step != start_vertex:
+        print(current_step, path_vertices[current_step])
+        path.insert(0, path_vertices[current_step].name)
+        current_step = path_vertices[current_step].name
+    return path
+
 # Solve maze using Pre-Order DFS algorithm, terminate with solution
 def walk_depth_first(maze, start_vertex_id, target_vertex_id, max_depth = 5):
     start_vertex = gr.Vertex(start_vertex_id)
@@ -12,57 +21,85 @@ def walk_depth_first(maze, start_vertex_id, target_vertex_id, max_depth = 5):
     #Start with first vertex and add it to the stack
     current_options_stack = [start_vertex]
     path_vertices = {}
+    previous_vertex = start_vertex
 
     while current_options_stack:
         # take next vertex from the options stack
         current_vertex = current_options_stack.pop()
         # indicate the state on the vertex as Work in progress
-        current_vertex.state = gr.VertexState.Wip
         # get neighboring vertices that are not yet closed as dead end
         possible_next_steps = maze.get_neighbors(current_vertex.name)
         
         # if no more neighbors are found (except the previous edge), close the vertex
         if len(possible_next_steps) <= 1 and current_vertex.name != start_vertex_id:
             current_vertex.state = gr.VertexState.Closed
+            continue
         
         # if we have already investigated this vertex, skip this loop and move to the next
         if current_vertex.name in path_vertices.keys():
             continue
         
-        path_vertices[current_vertex.name] = current_vertex
+        path_vertices[current_vertex.name] = previous_vertex
+        previous_vertex = current_vertex
         
         # add the next possible steps to the stack
         for possible_step in possible_next_steps:
             current_options_stack.append(possible_step)
             # when target is part of the next steps, add the last vertex to the path and exit the loop
             if possible_step.name == target_vertex.name:
-                path_vertices[target_vertex.name] = target_vertex
+                path_vertices[target_vertex.name] = previous_vertex
                 print('Target vector reached !!!')
                 # now return a list of all visited items on the path (and remove the closed ones)
-                return [v for v in path_vertices.values() if v.state != gr.VertexState.Closed]
+                #return [v for v in path_vertices.values() if v.state != gr.VertexState.Closed]
+                return track_back(path_vertices, start_vertex_id, target_vertex_id)
 
     # When we arrive here, no valid path was found
     return None
+
+def walk_breadth_first(maze, start_vertex_id, target_vertex_id, max_depth = 5):
+    start_vertex = gr.Vertex(start_vertex_id)
+    target_vertex = gr.Vertex(target_vertex_id)
+
+    if start_vertex == target_vertex:
+        print('The target and start were equal.  Returning empty list')
+        return []
     
-def generate_maze(dimensions = 8):
-    maze = gr.Graph()
+    #Start with first vertex and add it to the stack
+    current_options_queue = [start_vertex]
+    path_vertices = {}
+    previous_vertex = start_vertex
 
-    # Add vertices for all other (numeric) nodes
-    for i in range(0, 16):
-        maze.add_vertex(gr.Vertex(i)) #f"{i:02d}"))
+    while current_options_queue:
+        # take next vertex from the options stack
+        current_vertex = current_options_queue[0]
+        current_options_queue.remove(current_options_queue[0])
+        # indicate the state on the vertex as Work in progress
+        # get neighboring vertices that are not yet closed as dead end
+        possible_next_steps = maze.get_neighbors(current_vertex.name)
+        
+        # if no more neighbors are found (except the previous edge), close the vertex
+        if len(possible_next_steps) <= 1 and current_vertex.name != start_vertex_id:
+            current_vertex.state = gr.VertexState.Closed
+            continue
+        
+        # if we have already investigated this vertex, skip this loop and move to the next
+        if current_vertex.name in path_vertices.keys():
+            continue
+        
+        path_vertices[current_vertex.name] = previous_vertex
+        previous_vertex = current_vertex
+        
+        # add the next possible steps to the stack
+        for possible_step in possible_next_steps:
+            current_options_queue.append(possible_step)
+            # when target is part of the next steps, add the last vertex to the path and exit the loop
+            if possible_step.name == target_vertex.name:
+                path_vertices[target_vertex.name] = previous_vertex
+                print('Target vector reached !!!')
+                # now return a list of all visited items on the path (and remove the closed ones)
+                #return [v for v in path_vertices.values() if v.state != gr.VertexState.Closed]
+                return track_back(path_vertices, start_vertex_id, target_vertex_id)
 
-    # Add all edge (0 = S - 15 = T)
-    edges = ['02:10', '02:11', '02:01', '01:03', '03:12', '03:04', '04:13', '04:05', '05:06', '05:09', '09:15', '09:07', '05:06', '06:07', '06:00', '01:00', '08:00', '08:14']
-    edges = ['02:10', '02:11', '02:01', '01:03', '03:12', '03:04', '04:13', '04:05', '05:06', '05:09', '09:15', '09:07', '05:06', '06:07', '08:07', '08:00', '08:14']
-    for edge in edges:
-        maze.add_edge(int(edge.split(':')[0]), int(edge.split(':')[1]))
-    return maze
-
-#maze = generate_maze()
-#maze.print_graph()
-#for n in maze.get_neighbors(3):
-#    n.print()
-#walked_path_vertices = walk_depth_first(maze, 0, 15)
-#for path_vertex in walked_path_vertices:
-#    path_vertex.print()
-#print(walked_path_vertices)
+    # When we arrive here, no valid path was found
+    return None
+   
--- src/04-graph-algorithms/test_graph.py
+++ src/04-graph-algorithms/test_graph.py
@@ -15,13 +15,21 @@ class TestMethods(unittest.TestCase):
     def test_neighbors_standalone(self):
         self.validate_neighbors(12, 0, ['02:10', '10:03', '03:04'])
 
-    def test_successful_walks(self):
+    def test_successful_df_walks(self):
         self.validate_depth_first(0, 15, True, ['02:10', '02:11', '02:01', '01:03', '03:04', '04:13', '04:05', '05:06', '05:09', '09:15', '09:07', '05:06', '06:07', '06:00', '01:00', '08:00', '08:14'])
         self.validate_depth_first(0, 15, True, ['00:10', '10:05', '05:08', '08:11', '10:15', '11:13'], 3)
 
-    def test_failed_walks(self):
+    def test_failed_df_walks(self):
         self.validate_depth_first(0, 15, False, ['00:10', '10:05', '05:08', '08:11', '10:14', '11:13', '09:15'])
         self.validate_depth_first(0, 15, False, [])
+
+    def test_successful_bf_walks(self):
+        self.validate_breadth_first(0, 15, True, ['02:10', '02:11', '02:01', '01:03', '03:04', '04:13', '04:05', '05:06', '05:09', '09:15', '09:07', '05:06', '06:07', '06:00', '01:00', '08:00', '08:14'])
+        self.validate_breadth_first(0, 15, True, ['00:10', '10:05', '05:08', '08:11', '10:15', '11:13'])
+
+    def test_failed_bf_walks(self):
+        self.validate_breadth_first(0, 15, False, ['00:10', '10:05', '05:08', '08:11', '10:14', '11:13', '09:15'])
+        self.validate_breadth_first(0, 15, False, [])
 
     def create_maze(self, maze_edges):
         maze = gr.Graph()
@@ -41,6 +49,12 @@ class TestMethods(unittest.TestCase):
         if(exact_steps >= 0):
             self.assertEqual(exact_steps, len(walked_path_vertices))
 
+    def validate_breadth_first(self, start, target, should_find_path, maze_edges, exact_steps = -1):
+        maze = self.create_maze(maze_edges)
+        walked_path_vertices = gs.walk_breadth_first(maze, start, target)
+        self.assertEqual(should_find_path, walked_path_vertices!=None)
+        if(exact_steps >= 0):
+            self.assertEqual(exact_steps, len(walked_path_vertices))
 
     def validate_neighbors(self, vertex, expected_neighbors, maze_edges):
         maze = self.create_maze(maze_edges)

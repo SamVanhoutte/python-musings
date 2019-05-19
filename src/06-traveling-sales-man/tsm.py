9c54b5091af9eb85a59999392cbe4c9e07845525
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2019-05-19 17:07:16 +0200

    added 3d-test and plotting

--- src/06-traveling-sales-man/tsm.py
+++ src/06-traveling-sales-man/tsm.py
@@ -4,31 +4,34 @@ import route as rt
 import city
 import random
 import math
+import matplotlib.pyplot as plt
 
-city01 = city.City('01', 0, 2)
-city02 = city.City('02', 4, 5)
-city03 = city.City('03', 4, 2)
-city04 = city.City('04', 7, 3)
-city05 = city.City('05', 1, 6)
-city06 = city.City('06', 3, 2)
-city07 = city.City('07', 10, 8)
-city08 = city.City('08', 6, 3)
+pop_size = 100
+generation_count = 100
+
+# Hardcoded cities
+city01 = city.City('01', 0, 2, 4)
+city02 = city.City('02', 4, 5, 2)
+city03 = city.City('03', 4, 2, 8)
+city04 = city.City('04', 7, 3, 9)
+city05 = city.City('05', 1, 6, -1)
+city06 = city.City('06', 3, -2, -5)
+city07 = city.City('07', 10, 8, 3)
+city08 = city.City('08', -6, 3, 8)
 genes = [city01, city02, city03, city04, city05, city06, city07, city08]
 
-genes = []
-route_length = 10
-for i in range(0, route_length):
-    genes.append(city.City(str(i), i, i))
-print ('The shortest possible distance is ',  math.sqrt((route_length-1)**2 * 2) * 2)
-pop_size = 100
+# Generating cities in one straight line
+# genes = []
+# route_length = 25
+# for i in range(0, route_length):
+#     genes.append(city.City(str(i), i, i))
+# print ('The shortest possible distance is ',  math.sqrt((route_length-1)**2 * 2) * 2)
 
 routes = []
-generation_count = 30
-
 for i in range(0, pop_size):
     routes.append(rt.Route(random.sample(genes, len(genes))))
     
-population = gen.Population(routes, pop_size, pop_size // 2)
+population = gen.Population(routes, pop_size, pop_size // 3)
 
 distances = [population.get_fastest_route().get_distance()]
 for _ in range(0, generation_count):
@@ -37,3 +40,10 @@ for _ in range(0, generation_count):
     fastest = population.get_fastest_route()
     distances.append(fastest.get_distance())
     fastest.render()
+
+plt.plot(range(0, generation_count + 1), distances)
+plt.title('Distance of the path for every generation')
+plt.ylabel('Distance')
+plt.ylabel('Generation')
+# plt.axis([0, generation_count + 1, 0, 300])
+plt.show()

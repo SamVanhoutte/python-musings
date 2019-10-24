b06c4f30f9bc98bf7f6bf9c404f96d999c00c239
sam.vanhoutte@marchitec.be <sam.vanhoutte@marchitec.be>
2019-10-24 10:46:40 +0200

    nltk pyramid

--- src/nlp/nltk/01-ntlk-pyramid.py
+++ src/nlp/nltk/01-ntlk-pyramid.py
@@ -1,20 +1,11 @@
 from nltk import word_tokenize, pos_tag, ne_chunk
-from nltk.stem import PorterStemmer 
-from nltk.tag import untag, str2tuple, tuple2str
 from nltk.chunk import tree2conllstr, conllstr2tree, conlltags2tree, tree2conlltags
-from nltk.tag.stanford import POSTagger
 import nltk
 
-text = "Drive me from Seattle to Brussels"
-# Morphology - tagging the words
+text = "Fly me from Seattle to Brussels"
 tokens = word_tokenize(text)
-        
-# Part of speech tagging
 tagged_tokens = pos_tag(tokens)
-tagged_tokens = POSTagger(tokens)
-# Create named entity tree of tagged tokens
 ner_tree = ne_chunk(tagged_tokens)
-        
-# Get tag structure
+print(ner_tree)        
 iob_tagged = tree2conlltags(ner_tree)
 print(iob_tagged)

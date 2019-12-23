cff5afa8c3db9369234a3f140fd3dcc21c069584
Sam Vanhoutte <samvanhoutte@Sams-MBP.home>
2019-12-23 12:18:30 +0100

    remove_samples

--- src/nlp/chatbot/sample/botbuilder-python-master/libraries/botbuilder-testing/botbuilder/testing/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from .dialog_test_client import DialogTestClient
-from .dialog_test_logger import DialogTestLogger
-
-
-__all__ = ["DialogTestClient", "DialogTestLogger"]
